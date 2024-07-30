d565435da734b6941fa20b6443f2fd31e4d688d2
Betul Tugce Dikdogmus <betultugcedikdogmus@gmail.com>
2024-07-30 11:23:50 +0300

    Feature alanı dinamik hale getirildi.

--- MyPortfolio/ViewComponents/_FeatureComponentPartial.cs
+++ MyPortfolio/ViewComponents/_FeatureComponentPartial.cs
@@ -1,9 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Contexts;
 
 namespace MyPortfolio.ViewComponents
 {
     public class _FeatureComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke() { return View(); }
+        private readonly MyPortfolioDbContext _context;
+
+        public _FeatureComponentPartial(MyPortfolioDbContext context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke() 
+        { 
+            var values = _context.Features.ToList();
+            return View(values); 
+        }
     }
 }
