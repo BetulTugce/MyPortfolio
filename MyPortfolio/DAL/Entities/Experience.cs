5d7acfd325eed5841a46af0040bd779f2cf5633b
Betul Tugce Dikdogmus <betultugcedikdogmus@gmail.com>
2024-07-31 10:15:14 +0300

    Deneyim alanı dinamik hale getirildi.

--- MyPortfolio/DAL/Entities/Experience.cs
+++ MyPortfolio/DAL/Entities/Experience.cs
@@ -7,6 +7,8 @@ namespace MyPortfolio.DAL.Entities
         public string Company { get; set; }
         public string Position { get; set; }
         public string Description { get; set; }
-        public string Date { get; set; }
+        //public string Date { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
     }
 }
--- MyPortfolio/ViewComponents/_ExperienceComponentPartial.cs
+++ MyPortfolio/ViewComponents/_ExperienceComponentPartial.cs
@@ -1,9 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Contexts;
 
 namespace MyPortfolio.ViewComponents
 {
     public class _ExperienceComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke() {  return View(); }
+        private readonly MyPortfolioDbContext _context;
+        public _ExperienceComponentPartial(MyPortfolioDbContext context)
+        {
+            _context = context;
+        }
+        public IViewComponentResult Invoke()
+        {  
+            var values = _context.Experiences.ToList();
+            return View(values);
+        }
     }
 }
