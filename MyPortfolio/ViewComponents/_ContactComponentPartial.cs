43ae2c978d5084f837a91e88557f6fad521c7ab4
Betul Tugce Dikdogmus <betultugcedikdogmus@gmail.com>
2024-08-01 16:50:06 +0300

    İletişim alanı dinamik hale getirildi.

--- MyPortfolio/ViewComponents/_ContactComponentPartial.cs
+++ MyPortfolio/ViewComponents/_ContactComponentPartial.cs
@@ -1,9 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Contexts;
 
 namespace MyPortfolio.ViewComponents
 {
     public class _ContactComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke() {  return View(); }
+        private readonly MyPortfolioDbContext _context;
+
+        public _ContactComponentPartial(MyPortfolioDbContext context)
+        {
+            _context = context;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var value = _context.Contacts.FirstOrDefault();
+            return View(value); 
+        }
     }
 }
