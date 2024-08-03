bb8f853a9a010a9f1437573a47f47b0dd4ec0f56
Betul Tugce Dikdogmus <betultugcedikdogmus@gmail.com>
2024-08-03 13:14:10 +0300

    Testimonials alanı dinamik hale getirildi.

--- MyPortfolio/ViewComponents/_TestimonialComponentPartial.cs
+++ MyPortfolio/ViewComponents/_TestimonialComponentPartial.cs
@@ -1,9 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Contexts;
 
 namespace MyPortfolio.ViewComponents
 {
     public class _TestimonialComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke() { return View(); }
+        private readonly MyPortfolioDbContext _context;
+
+        public _TestimonialComponentPartial(MyPortfolioDbContext context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke() 
+        {
+            var values = _context.Testimonials.ToList();
+            return View(values); 
+        }
     }
 }
