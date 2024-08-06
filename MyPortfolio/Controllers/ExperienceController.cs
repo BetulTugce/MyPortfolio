78ced4734971683c9bb97cd79aec95219e53a5a2
Betul Tugce Dikdogmus <betultugcedikdogmus@gmail.com>
2024-08-06 14:28:24 +0300

    Deneyim güncelleme işlemi gerçekleştirildi.

--- MyPortfolio/Controllers/ExperienceController.cs
+++ MyPortfolio/Controllers/ExperienceController.cs
@@ -40,5 +40,20 @@ namespace MyPortfolio.Controllers
             _context.SaveChanges();
             return RedirectToAction("ExperienceList");
         }
+
+        [HttpGet]
+        public IActionResult UpdateExperience(int id) 
+        {
+            var value = _context.Experiences.Find(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateExperience(Experience experience)
+        {
+            _context.Experiences.Update(experience);
+            _context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
     }
 }
