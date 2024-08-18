b85c23c2ae516132c3907d16090b0b1587243843
Betul Tugce Dikdogmus <betultugcedikdogmus@gmail.com>
2024-08-18 21:45:28 +0300

    Admin panelindeki istatistik alanı dinamik hale getirildi.

---
+++ MyPortfolio/Controllers/StatisticController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Contexts;
+
+namespace MyPortfolio.Controllers
+{
+    public class StatisticController : Controller
+    {
+		private readonly MyPortfolioDbContext _context;
+
+        public StatisticController(MyPortfolioDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.SkillCount = _context.Skills.Count();
+            ViewBag.MessageCount = _context.Messages.Count(i => !i.IsRead);
+            ViewBag.ExperienceCount = _context.Experiences.Count();
+			return View();
+        }
+    }
+}
