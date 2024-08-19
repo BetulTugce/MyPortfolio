2e1ab7774c2d0167c716e538ee26077ef8a406d7
Betul Tugce Dikdogmus <betultugcedikdogmus@gmail.com>
2024-08-19 20:07:55 +0300

    Mesajları listeleme işlemi gerçekleştirildi.

---
+++ MyPortfolio/Controllers/MessageController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Contexts;
+
+namespace MyPortfolio.Controllers
+{
+	public class MessageController : Controller
+	{
+		private readonly MyPortfolioDbContext _context;
+
+        public MessageController(MyPortfolioDbContext context)
+        {
+			_context = context;
+        }
+
+        public IActionResult Inbox()
+		{
+			var values = _context.Messages.ToList();
+			return View(values);
+		}
+	}
+}
